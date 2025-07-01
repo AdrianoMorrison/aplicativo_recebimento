f474460de6ab74a53677b7be25763c6b7ccccbbf
AdrianoMorrison <165274741+AdrianoMorrison@users.noreply.github.com>
2025-07-01 16:07:03 -0300

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -2,7 +2,7 @@ import pyodbc
 
 def get_connection():
     connection = pyodbc.connect(
-        "Driver={SQL Server};"
+        "Driver={ODBC Driver 17 for SQL Server};"
         "Server=192.168.9.100;"  # Atualize para o seu servidor
         "Database=DB_VIEWS;"       # Atualize para o seu banco de dados
         "UID=Logistica_OPCD;"
